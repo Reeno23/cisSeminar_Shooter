de4cb01bb26e8b3ee8d5d9947f8ca382a7191b24
Ryan Smalley <flaxicon@gmail.com>
2016-04-22 17:33:44 -0400

    Scoring, new game, quit, and background
    Added working game over screen, a background, a floating score display,
    quit button that should work once its an exe.
    Notable issues: Walls and blue speed circles go over all UI stuff.

---
+++ Assets/Scripts/Using/Score.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+    public int score = 0;
+
+    Text scoreDisplay;
+    void Start()
+    {
+        scoreDisplay = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        score = (GlobalStats.enemiesKilled * 10) + GlobalStats.stopwatch.Elapsed.Seconds;
+        scoreDisplay.text="Score: " + score;
+    }
+}
